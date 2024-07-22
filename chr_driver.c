88600f46f5a627ad71addce8fdca86e1b38668bf
SuvorovDm-1 <dim-suv@mail.ru>
2024-07-22 03:07:52 +0300

    The first version of the read and write operations

--- chr_driver.c
+++ chr_driver.c
@@ -3,12 +3,45 @@
 #include <linux/fs.h>
 #include <linux/types.h>
 #include <linux/cdev.h>
+#include <linux/semaphore.h>
+#include <linux/wait.h>
+#include <linux/sched.h>
+#include <linux/slab.h>
 #include <linux/uaccess.h>
+#include <linux/uidgid.h>
+#include <linux/cred.h>
+
 #define DEV_NAME "my_char_driver"
+#define INITIAL_BUFFER_SIZE 512
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Suvorov Dmitriy dim-suv@mail.ru");
 MODULE_DESCRIPTION("A character driver with a shared ring buffer");
+
+static int buffer_size = INITIAL_BUFFER_SIZE;
+module_param(buffer_size, int, S_IRUGO);
+
+struct ring_buffer {
+    char* data;
+    int write_p;
+    int read_p;
+    int size;
+    struct semaphore sem;
+    wait_queue_head_t read_queue;
+    wait_queue_head_t write_queue;
+};
+
+struct last_operation_info {
+    unsigned long last_read_time;
+    unsigned long last_write_time;
+    pid_t read_proc_id;
+    pid_t write_proc_id;
+    kuid_t read_user_id;
+    kuid_t write_user_id;
+};
+
+static struct ring_buffer* r_buf;
+static struct last_operation_info rw_info;
 
 static dev_t    dev_nums;
 static struct   cdev my_char_dev;
@@ -33,9 +66,30 @@ static int __init my_char_driver_init(void) {
 
     printk(KERN_INFO "Initializing %s\n", DEV_NAME);
 
+    // buffer initializing
+    r_buf = kmalloc(sizeof(struct ring_buffer), GFP_KERNEL);
+    if (!r_buf) {
+        printk(KERN_ERR "Failed to allocate memory for the ring buffer\n");
+        return -ENOMEM;
+    }
+    r_buf->data = kmalloc(buffer_size, GFP_KERNEL);
+    if (!r_buf->data) {
+        kfree(r_buf);
+        printk(KERN_ERR "Failed to allocate memory for buffer data\n");
+        return -ENOMEM;
+    }
+    r_buf->write_p = 0;
+    r_buf->read_p = 0;
+    r_buf->size = buffer_size;
+    sema_init(&r_buf->sem, 1);
+    init_waitqueue_head(&r_buf->read_queue);
+    init_waitqueue_head(&r_buf->write_queue);
+
     // Try to get a major device number
     success = alloc_chrdev_region(&dev_nums, 0, 1, DEV_NAME);
     if (success < 0) {
+        kfree(r_buf->data);
+        kfree(r_buf);
         printk(KERN_ERR "Error registering major number for the device %s\n", DEV_NAME);
         return success;
     }
@@ -48,6 +102,8 @@ static int __init my_char_driver_init(void) {
     success = cdev_add(&my_char_dev, dev_nums, 1);
     if (success < 0) {
         unregister_chrdev_region(dev_nums, 1);
+        kfree(r_buf->data);
+        kfree(r_buf);
         printk(KERN_ERR "Failed to add character device\n");
         return success;
     }
@@ -59,6 +115,8 @@ static int __init my_char_driver_init(void) {
 static void __exit my_char_driver_exit(void) {
     cdev_del(&my_char_dev);
     unregister_chrdev_region(dev_nums, 1);
+    kfree(r_buf->data);
+    kfree(r_buf);
     printk(KERN_INFO "Goodbye from the %s!\n", DEV_NAME);
 }
 
@@ -72,13 +130,97 @@ static int c_dev_release(struct inode* inode_p, struct file* file_p) {
     return 0;
 }
 
-static ssize_t c_dev_read(struct file* file_p, char* buffer, size_t len, loff_t* offset) {
-    printk(KERN_INFO "Read operation\n");
-    return 0;
+static ssize_t c_dev_read(struct file* file_p, char __user* buffer, size_t len, loff_t* offset) {
+    if (down_interruptible(&r_buf->sem))
+        return -ERESTARTSYS;
+
+    while (r_buf->write_p == r_buf->read_p) { /* while nothing to read */
+        up(&r_buf->sem);
+
+        // Nonblocking reading
+        if (file_p->f_flags & O_NONBLOCK)
+            return -EAGAIN;
+
+        // Blocking reading
+        if (wait_event_interruptible(r_buf->read_queue, (r_buf->write_p != r_buf->read_p)))
+            return -ERESTARTSYS;
+
+        if (down_interruptible(&r_buf->sem))
+            return -ERESTARTSYS;
+    }
+
+    /* there is some data to read */
+    if (r_buf->write_p > r_buf->read_p)
+        len = min(len, (size_t)(r_buf->write_p - r_buf->read_p));
+    else
+        len = min(len, (size_t)(r_buf->size - r_buf->read_p));
+
+    if (copy_to_user(buffer, r_buf->data + r_buf->read_p, len)) {
+        up(&r_buf->sem);
+        return -EFAULT;
+    }
+
+    r_buf->read_p += len;
+    if (r_buf->read_p == r_buf->size)
+        r_buf->read_p = 0;
+
+    rw_info.last_read_time = jiffies;
+    rw_info.read_proc_id = current->pid;
+    rw_info.read_user_id = current_uid();
+
+    up(&r_buf->sem);
+
+    // waking up the writers
+    wake_up_interruptible(&r_buf->write_queue);
+
+    printk(KERN_INFO "read_op: %zu bytes were read\n", len);
+    return len;
 }
 
-static ssize_t c_dev_write(struct file* file_p, const char* buffer, size_t len, loff_t* offset) {
-    printk(KERN_INFO "Write operation\n");
+static ssize_t c_dev_write(struct file* file_p, const char __user* buffer, size_t len, loff_t* offset) {
+    if (down_interruptible(&r_buf->sem))
+        return -ERESTARTSYS;
+
+    while ((r_buf->write_p + 1) % r_buf->size == r_buf->read_p) { // no place to write
+        up(&r_buf->sem);
+
+        // Nonblocking writing
+        if (file_p->f_flags & O_NONBLOCK)
+            return -EAGAIN;
+
+        // Blocking writing
+        if (wait_event_interruptible(r_buf->write_queue, ((r_buf->write_p + 1) % r_buf->size != r_buf->read_p)))
+            return -ERESTARTSYS;
+
+        if (down_interruptible(&r_buf->sem))
+            return -ERESTARTSYS;
+    }
+
+    // There is some place to write
+    if (r_buf->write_p < r_buf->read_p)
+        len = min(len, (size_t)(r_buf->read_p - r_buf->write_p - 1));
+    else
+        len = min(len, (size_t)(r_buf->size - r_buf->write_p - 1));
+
+    if (copy_from_user(r_buf->data + r_buf->write_p, buffer, len)) {
+        up(&r_buf->sem);
+        return -EFAULT;
+    }
+
+    r_buf->write_p += len;
+    if (r_buf->write_p == r_buf->size)
+        r_buf->write_p = 0;
+
+    rw_info.last_write_time = jiffies;
+    rw_info.write_proc_id = current->pid;
+    rw_info.write_user_id = current_uid();
+
+    up(&r_buf->sem);
+
+    // waking up the readers
+    wake_up_interruptible(&r_buf->read_queue);
+
+    printk(KERN_INFO "write_op: %zu bytes were written\n", len);
     return len;
 }
 
